07d8d91bb6f3472477325580beae1be4ffff65e3
Hagill <youmu12589@gmail.com>
2025-11-13 15:17:54 +0900

    feat: 탑 사이 레이저에 접촉 시 다리 생성 효과

---
+++ Assets/Scripts/EnvironmentObject/Tower/TowerLaser.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLaser : MonoBehaviour
+{
+    [Header("TowerLaser")]
+    public Transform laserOrigin;
+    public Transform laserTarget;
+    public LineRenderer lineRenderer;
+    public float laserWidth;
+    public LayerMask playerLayer;
+
+    [Header("Effect")]
+    public float detectionDelay;
+    public ParticleSystem smokeParticleSystem;
+    public GameObject bridgeObject;
+    public float bridgeAppearTime;
+
+    [Header("ScreenVibration")]
+    public Camera camera;
+    public float shakeDuration;
+    public float shakeMagnitude;
+
+    private bool trapActivated = false;
+    private bool playerDetected = false;
+
+    private Vector3 offsetOrigin;
+    private Vector3 offsetTarget;
+    public float yOffset = 1f;
+
+    private void Start()
+    {
+        offsetOrigin = laserOrigin.position;
+        offsetTarget = laserTarget.position;
+
+        offsetOrigin.y += yOffset;
+        offsetTarget.y += yOffset;
+
+        if (lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        lineRenderer.startWidth = laserWidth;
+        lineRenderer.endWidth = laserWidth;
+        lineRenderer.positionCount = 2;
+
+        lineRenderer.SetPosition(0, offsetOrigin);
+        lineRenderer.SetPosition(1, offsetTarget);
+
+        if (bridgeObject != null)
+        {
+            bridgeObject.SetActive(false);
+        }
+
+        if (smokeParticleSystem != null)
+        {
+            smokeParticleSystem.gameObject.SetActive(false);
+        }
+
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        if (trapActivated) return;
+
+        RaycastHit hit;
+
+        if (Physics.Linecast(offsetOrigin, offsetTarget, out hit, playerLayer))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                if(!playerDetected)
+                {
+                    playerDetected = true;
+                    StartCoroutine(ActivateTrap());
+                }
+            }
+        }
+        else
+        {
+            if (playerDetected)
+            {
+                playerDetected = false;
+            }
+        }
+
+        Debug.DrawLine(offsetOrigin, offsetTarget, Color.red);
+    }
+
+    IEnumerator ActivateTrap()
+    {
+        trapActivated = true;
+
+        yield return new WaitForSeconds(detectionDelay);
+
+        if (camera != null)
+        {
+            StartCoroutine(ShakeCamera());
+        }
+
+        if (smokeParticleSystem != null)
+        {
+            smokeParticleSystem.gameObject.SetActive(true); // È°¼ºÈ­
+            smokeParticleSystem.Play();
+
+            StartCoroutine(SmokeDelay(smokeParticleSystem.main.duration + smokeParticleSystem.main.startLifetime.constantMax));
+        }
+
+        if(bridgeObject != null)
+        {
+            bridgeObject.SetActive(true);
+        }
+
+        lineRenderer.enabled = false;
+    }
+
+    IEnumerator SmokeDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (smokeParticleSystem != null)
+        {
+            smokeParticleSystem.gameObject.SetActive(false);
+            smokeParticleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+
+
+    IEnumerator ShakeCamera()
+    {
+        if (camera == null) yield break;
+
+        Vector3 originalPosition = camera.transform.localPosition;
+        float elapsed = 0f;
+
+        while(elapsed < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            camera.transform.localPosition = originalPosition + new Vector3(x, y, 0f);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        camera.transform.localPosition = originalPosition;
+    }
+}
